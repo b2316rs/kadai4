9eaae231bd50778906d02aff67f9cc892b4fc66d
b2316rs <b2316rs@aiit.ac.jp>
2023-07-26 22:08:17 +0000

    chnage gcd

--- gcd.sh
+++ gcd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "このスクリプトは2つの自然数を引数として必要とします."
+    echo "このスクリプトは2つの自然数を引数として必要とします"
     exit 1
 fi
 
 for num in $1 $2; do
     if ! [[ "$num" =~ ^[1-9][0-9]*$ ]]; then
-        echo "引数は自然数である必要があります.: $num"
+        echo "引数は自然数である必要があります: $num"
         exit 1
     fi
 done
